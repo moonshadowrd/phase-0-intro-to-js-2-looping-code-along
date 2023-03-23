7b66b9b3c10f60d3a09eeae2982df53323fbfd22
jose cabreja <cabreja@yahoo.com>
2023-03-22 21:46:34 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(name, value){
+    let myArray=[]
+    for(let i=0; i < name.length; i++) {
+        myArray.push("Thank you, Guadalupe, for the wonderful surprise gift!")
+        myArray.push("Thank you, Ollie, for the wonderful surprise gift!" )
+        myArray.push("Thank you, Aki, for the wonderful surprise gift!")
+        return myArray
+
+}
+}
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
